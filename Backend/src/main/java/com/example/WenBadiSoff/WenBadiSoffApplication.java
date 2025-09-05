2cc33275ea695b084a389925f5170a6925c07c6d
Hasan Kourani <hasankoura8@gmail.com>
2025-09-05 21:31:53 +0300

    Add read me file

---
+++ Backend/src/main/java/com/example/WenBadiSoff/WenBadiSoffApplication.java
@@ -0,0 +1,13 @@
+package com.example.WenBadiSoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WenBadiSoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WenBadiSoffApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/WenBadiSoff/WenBadiSoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WenBadiSoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WenBadiSoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
