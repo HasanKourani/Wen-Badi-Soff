0a9733b1a95015565010cb2d93531ef6bfef2b5d
Hasan Kourani <hasankoura8@gmail.com>
2025-09-06 15:37:20 +0300

    Create registerUser controller

--- Backend/src/main/java/com/example/WenBadiSoff/user/UserController.java
+++ Backend/src/main/java/com/example/WenBadiSoff/user/UserController.java
@@ -1,13 +1,24 @@
 package com.example.WenBadiSoff.user;
 
+import com.example.WenBadiSoff.user.model.User;
+import com.example.WenBadiSoff.user.services.RegisterUserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final RegisterUserService registerUserService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(RegisterUserService registerUserService) {
+        this.registerUserService = registerUserService;
+    }
+
+
+    @PostMapping("/user/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        return registerUserService.execute(user);
     }
 }
